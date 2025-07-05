da71b08a617460581662cb68c98bda8ed8401fdf
harithsaid <109976464+harithsaid@users.noreply.github.com>
2025-07-05 18:27:59 +0300

    Add files via upload

---
+++ run_pipeline.py
@@ -0,0 +1,6 @@
+from pipelines.train_pipeline import train_pipeline
+
+if __name__ == "__main__":
+    # Define the path to your data
+     # Run the pipeline
+    train_pipeline(data_path = "C:/Users/hsaid/Downloads/ZENML_blueprint/data/olist_customers_dataset.csv" )
